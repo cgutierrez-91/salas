5b4fb18ec02a8da18ddfb920ad9dc0e62b0e7e32
Cesar Gutierrez <cgutierrezmh@gmail.com>
2019-07-26 08:17:35 -0600

    agregar KEY CONFIG

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    //'url' => env('APP_URL', 'http://localhost'),
+	'url' => env('APP_URL', 'https://reservacion-sala.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -119,7 +120,8 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    //'key' => env('APP_KEY'),
+	'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
 
